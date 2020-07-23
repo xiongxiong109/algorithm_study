160856740208dca45ba0228df5a5b55b0da77a03
jqxiong <jqxiong@trip.com>
2020-07-23 21:01:10 +0800

    linklist

---
+++ py/__tests__/linklist_test.py
@@ -0,0 +1,18 @@
+import unittest
+from data_structures.linklist import ListNode, Linklist
+
+class TestLinkList(unittest.TestCase):
+    # 新增节点方法测试
+    def test_node(self):
+        node = ListNode(item = { 'foo': 'bar' })
+        self.assertEqual(node.item, { 'foo': 'bar' })
+        self.assertEqual(node.next, None)
+    # 链表插入方法测试
+    def test_insert(self):
+        li = Linklist()
+        # 按序插入
+        li.insert('hello', 'head')
+        li.insert('world', 'hello')
+        # 在指定元素后面插入
+        li.insert('xiong', 'world')
+        self.assertEqual(li.toArr(), ['hello', 'world', 'xiong'])
---
+++ py/data_structures/linklist.py
@@ -0,0 +1,39 @@
+# 链表数据结构实现
+# 一种元素之间有节点关联的数据结构，可以顺着节点往下索引，但不一定是数组
+# 这种实现可以没有任何地方存储原始数据，完全存入内存
+
+class Linklist():
+    def __init__(self):
+        # 创建一个头节点
+        self.head = ListNode()
+    # 在指定的item后面插入item
+    def insert(self, newItem, item = None):
+        # 找到目标元素
+        pre_item = self.find(item)
+        # 创建新的节点，并且目标元素的指针指向新的节点
+        next_item = ListNode(newItem)
+        next_item.next = pre_item.next
+        pre_item.next = next_item
+    # 查找指定节点
+    def find(self, item):
+        # 从头部开始遍历
+        cur_item = self.head
+        while cur_item.item != item:
+            cur_item = cur_item.next
+        return cur_item
+        
+    # 遍历展示链表里的元素
+    def toArr(self):
+        node = self.head
+        arr = []
+        while(node.next != None):
+            arr.append(node.next.item)
+            # 指针后移
+            node = node.next
+        return arr
+
+# 每一个节点拥有next的指针
+class ListNode(object):
+    def __init__(self, item = 'head'):
+        self.item = item
+        self.next = None

1d43448d161edc9708569326b2c78cfc572afd88
jqxiong <jqxiong@trip.com>
2020-07-30 15:43:09 +0800

    添加remove 测试

--- py/__tests__/db_linklist_test.py
+++ py/__tests__/db_linklist_test.py
@@ -10,3 +10,13 @@ class TestDbLinkList(unittest.TestCase):
         li.insert('world', 'hello')
         li.insert('xiong', 'world')
         self.assertEqual(li.to_arr(), ['hello', 'world', 'xiong'])
+
+    # 删除方法
+    def test_remove(self):
+        li = DBLinkList()
+        li.insert('hello', 'head')
+        li.insert('bear', 'hello')
+        li.insert('xiong', 'bear')
+        self.assertEqual(li.to_arr(), ['hello', 'bear', 'xiong'])
+        li.remove('bear')
+        self.assertEqual(li.to_arr(), ['hello', 'xiong'])

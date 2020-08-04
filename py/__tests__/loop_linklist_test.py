d75a0bdfc20edb2d419d2a22e4782c7a9142576f
jqxiong <jqxiong@trip.com>
2020-08-04 10:29:13 +0800

    add loop list method

--- py/__tests__/loop_linklist_test.py
+++ py/__tests__/loop_linklist_test.py
@@ -9,3 +9,16 @@ class TestLoopLinkList(unittest.TestCase):
         li.insert('bear')
         li.insert('xiong', 'hello')
         self.assertEqual(li.to_arr(), ['bear', 'hello', 'xiong'])
+
+    def test_move(self):
+        li = LoopLinkList()
+        li.insert('leoo')
+        li.insert('bear')
+        li.insert('xx')
+        self.assertEqual(li.to_arr(), ['xx', 'bear', 'leoo'])
+        li.move(2)
+        self.assertEqual(li.cur_node.item, 'bear')
+        li.move_to_head()
+        self.assertEqual(li.cur_node.item, 'head')
+        li.move(4)
+        self.assertEqual(li.cur_node.item, 'xx')
--- py/data_structures/loop_linklist.py
+++ py/data_structures/loop_linklist.py
@@ -7,6 +7,8 @@ class LoopLinkList(LinkList):
     def __init__(self):
         super().__init__()
         # 构建循环链表
+        # 链表指针
+        self.cur_node = self.head
         self.head.next = self.head
 
     # 修改to_arr方法
@@ -17,3 +19,20 @@ class LoopLinkList(LinkList):
             arr.append(cur_node.next.item)
             cur_node = cur_node.next
         return arr
+    
+    # 指针向前移动n个位置
+    def move(self, n):
+        i = 0
+        while i < n:
+            i += 1
+            self.cur_node = self.cur_node.next
+        # 处理移动到头部后，自动走到下一位
+        if self.cur_node.item == 'head' and self.cur_node.next.item != 'head':
+            self.cur_node = self.cur_node.next
+
+    # 回到头部
+    def move_to_head(self):
+        self.cur_node = self.head
+
+    def show(self):
+        return self.cur_node

600cad2413e7a8058a290dc3b4a0cd08042f539c
jqxiong <jqxiong@trip.com>
2020-08-12 10:57:25 +0800

    线性探测法解决散列碰撞

--- py/__tests__/hash_table_test.py
+++ py/__tests__/hash_table_test.py
@@ -7,3 +7,14 @@ class TestHashTable(unittest.TestCase):
         hash_table = HashTable()
         hash_key = hash_table.put('hello')
         self.assertEqual(hash_table.table[hash_key], 'hello')
+
+    def test_conflict(self):
+        hash_table = HashTable()
+        hello_key = hash_table.put('helloa')
+        llohe_key = hash_table.put('llohae')
+        self.assertEqual(hello_key + 1, llohe_key)
+        # hellob 的hash_key 为 helloa + 1, 会与已经产生过碰撞的llohae再次冲突
+        db_conf_key = hash_table.put('hellob')
+        self.assertEqual(db_conf_key, hello_key + 2)
+        self.assertEqual(db_conf_key, llohe_key + 1)
+        self.assertEqual(hash_table.table[db_conf_key], 'hellob')
--- py/data_structures/hash_table.py
+++ py/data_structures/hash_table.py
@@ -5,15 +5,26 @@ class HashTable:
     HASH_KEY = 137
 
     def __init__(self):
-        self.table = ['' for item in range(self.HASH_KEY)]
+        self.table = [None for item in range(self.HASH_KEY)]
 
-    def hash(self, data=''):
+    def hash(self, data=None):
         total = 0
         for char in data:
             total += ord(char)
         return total % self.HASH_KEY
 
+    # 使用线性探测法解决碰撞问题(开链法在js中实现)
+    # 线性探测法基于一个事实，就是在散列表中，会存在很多的空位
+    # 当插入遇到碰撞的时候，往下一个增量的空位排一下就好了
+    # 当散列表的容量足够大(大于待存储数量的2倍以上)时，更适合使用线性探测
+    # 当存储容量在1.5倍之间时，使用开链法扩容
     def put(self, data=''):
         hash_key = self.hash(data)
-        self.table[hash_key] = data
-        return hash_key
+        final_hash_key = hash_key
+        if self.table[hash_key] is None:
+            self.table[hash_key] = data
+        else:
+            while self.table[final_hash_key] is not None:
+                final_hash_key += 1
+            self.table[final_hash_key] = data
+        return final_hash_key

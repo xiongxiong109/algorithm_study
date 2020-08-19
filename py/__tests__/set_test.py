9ee00bf589171ac8ab30999178049ab5c980c046
jqxiong <jqxiong@trip.com>
2020-08-19 10:48:44 +0800

    py intersection

--- py/__tests__/set_test.py
+++ py/__tests__/set_test.py
@@ -1,4 +1,4 @@
-from algorithm.set_util import union
+from algorithm.set_util import union, intersection
 import unittest
 
 
@@ -7,3 +7,7 @@ class TestSet(unittest.TestCase):
     def test_union(self):
         union_arr = union([1, 2, 4], [3, 2, 4, 5])
         self.assertEqual(union_arr, [1, 2, 3, 4, 5])
+
+    def test_inter(self):
+        inter_arr = intersection([1, 2, 4], [2, 3, 4], [3, 4, 5, 2])
+        self.assertEqual(inter_arr, [2, 4])
--- py/algorithm/set_util.py
+++ py/algorithm/set_util.py
@@ -1,6 +1,8 @@
 # python 中的set 集合类型也是无序，唯一的类型
 # python里的set自带子交并补集等方法
 
+from functools import reduce
+
 
 # 求并集
 def union(*args):
@@ -8,4 +10,16 @@ def union(*args):
     for item in args:
         set_arr = set_arr.union(item)
     return [item for item in set_arr]
+
+
+# 求交集
+def intersection(*args):
+    inter_sets = reduce(_inter_one, args)
+    return [item for item in inter_sets]
+
+
+def _inter_one(arg_a=[], arg_b=[]):
+    set_a = set(arg_a)
+    set_b = set(arg_b)
+    return set_a.intersection(set_b)
 

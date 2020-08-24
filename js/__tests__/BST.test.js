c85271021f5069f89e44f8d7048183bb7c86ae16
jqxiong <jqxiong@trip.com>
2020-08-24 10:10:15 +0800

    preOrder and postOrder

--- js/__tests__/BST.test.js
+++ js/__tests__/BST.test.js
@@ -1,6 +1,23 @@
 // BST测试
 const BST = require('../data_structures/BST')
 
+function createTree() {
+    const bst = new BST();
+    bst.insert(13)
+    bst.insert(55)
+    bst.insert(12)
+    bst.insert(2)
+    bst.insert(7)
+    bst.insert(19)
+    /**
+     *       13
+     *   12       55
+     * 2        19
+     *   7     
+     */
+    return bst
+}
+
 describe('BST', () => {
     it('插入 method', () => {
         const bst = new BST();
@@ -26,23 +43,24 @@ describe('BST', () => {
         expect(rightNode.right.data).toEqual(59)
     })
     it('中序遍历 method', () => {
-        const bst = new BST();
-        bst.insert(13)
-        bst.insert(55)
-        bst.insert(12)
-        bst.insert(2)
-        bst.insert(7)
-        bst.insert(19)
-        /**
-         *       13
-         *   12       55
-         * 2        19
-         *   7     
-         */
+        const bst = createTree()
         bst.midOrder(bst.root)
         expect(bst.show()).toEqual([2, 7, 12, 13, 19, 55])
     })
+    /**
+     *       13
+     *   12       55
+     * 2        19
+     *   7     
+     */
     it('先序遍历', () => {
-        
+        const bst = createTree();
+        bst.preOrder(bst.root);
+        expect(bst.show()).toEqual([13, 12, 2, 7, 55, 19]);
+    })
+    it('后序遍历', () => {
+        const bst = createTree();
+        bst.postOrder(bst.root)
+        expect(bst.show()).toEqual([7, 2, 12, 19, 55, 13])
     })
 })

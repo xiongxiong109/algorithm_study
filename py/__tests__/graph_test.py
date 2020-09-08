902f16a46827b2163b4afaa17064ab43926a13e8
jqxiong <jqxiong@trip.com>
2020-09-08 11:01:44 +0800

    py dfs

--- py/__tests__/graph_test.py
+++ py/__tests__/graph_test.py
@@ -2,12 +2,43 @@ import unittest
 from data_structures.graph import Graph
 
 
+def create_graph():
+    graph = Graph(['1', '2', '3', '4', '5'])
+    return graph
+
+
 class TestGraph(unittest.TestCase):
     def test_initMetrix(self):
-        graph = Graph(5)
+        graph = create_graph()
         # print(graph.metrix)
         graph.add_edge(3, 4)
         graph.add_edge(1, 2)
         graph.add_edge(2, 5)
-        print(graph.metrix)
+        # print(graph.metrix)
         self.assertEqual(graph.metrix[0], [0, 0, 0, 0, 0])
+
+    def test_dfs(self):
+        graph = create_graph()
+        graph.add_edge(1, 2)
+        graph.add_edge(0, 3)
+        graph.add_edge(3, 4)
+        graph.dfs(0)
+        self.assertEqual(graph.show_search_list(), [0, 3, 4])
+        #   01234
+        # 0 00010
+        # 1 00100
+        # 2 01000
+        # 3 10001
+        # 4 00010
+
+        graph.reset_search_list()
+        graph.add_edge(1, 3)
+        graph.dfs(0)
+        # print(graph.metrix)
+        #   01234
+        # 0 00010
+        # 1 00110
+        # 2 01000
+        # 3 11001
+        # 4 00010
+        self.assertEqual(graph.show_search_list(), [0, 3, 4, 1, 2])
--- py/data_structures/graph.py
+++ py/data_structures/graph.py
@@ -1,17 +1,23 @@
 # 图与图算法
 
-
 # 图类, 使用邻接列表或者邻接矩阵存储
 # 使用邻接列表方式存储已经在js中实现，py中尝试使用邻接矩阵
 class Graph:
-    def __init__(self, verticles=0):
-        self.verticles = verticles
+    def __init__(self, vertex_list):
+        self.verticles = len(vertex_list) or 0
         self.metrix = []
-        self.init_metrix()
+        self.vertex = {}
+        # 搜索后的排序数组
+        self.search_list = []
+        self.init_metrix(vertex_list)
 
     # 初始化邻接矩阵
-    def init_metrix(self):
+    def init_metrix(self, vertex_list):
         for i in range(0, self.verticles):
+            # 顺便将顶点初始化
+            if vertex_list and vertex_list[i]:
+                vertex = Vertex(data=vertex_list[i])
+                self.vertex["vertex%d" % i] = vertex
             self.metrix.append([])
             for j in range(0, self.verticles):
                 self.metrix[i].append(0)
@@ -20,11 +26,38 @@ class Graph:
     def add_edge(self, from_id, to_id):
         if from_id < self.verticles and to_id < self.verticles:
             self.metrix[from_id][to_id] = 1
-            # 如果是无向图, 可以同时给self.metrix[to_id][from_id] = 1
+            # self.vertex["vertex%d" % from_id].to_ids.append(to_id)
+            # 如果是无向图, 可以同时给下面这段赋值
+            self.metrix[to_id][from_id] = 1
+            # self.vertex["vertex%d" % to_id].from_ids.append(from_id)
+
+    # 重置搜索遍历点
+    def reset_search_list(self):
+        # 清空搜索数组
+        self.search_list = []
+        # 重置is_visited
+        for i in range(self.verticles):
+            self.vertex["vertex%d" % i].is_visited = False
 
     # 深度优先搜索
     def dfs(self, ver_id):
-        pass
+
+        cur_vertex = self.vertex["vertex%d" % ver_id]
+        cur_vertex.is_visited = True
+
+        # 横向遍历, for循环需要拿到索引的话, 需要用enumerate
+        for to_id, item in enumerate(self.metrix[ver_id]):
+            next_vertex = self.vertex["vertex%d" % to_id]
+            # 有连接点, 走到下一个分支去递归遍历
+            if item == 1 and not next_vertex.is_visited:
+                self.dfs(to_id)
+        self.search_list.append(ver_id)
+
+    def show_search_list(self):
+        arr = []
+        while len(self.search_list):
+            arr.append(self.search_list.pop())
+        return arr
 
 
 # 顶点类
@@ -32,5 +65,3 @@ class Vertex:
     def __init__(self, data=''):
         self.is_visited = False
         self.data = data
-        self.from_ids = []
-        self.to_ids = []

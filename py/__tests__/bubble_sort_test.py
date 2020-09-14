ef4a1d9eb330fd874b9f2e3e4564fc0adf27aac8
jqxiong <jqxiong@trip.com>
2020-09-14 10:23:05 +0800

    add selection sort

--- py/__tests__/bubble_sort_test.py
+++ py/__tests__/bubble_sort_test.py
@@ -1,10 +1,15 @@
 # 测试冒泡排序
 import unittest
-from algorithm.sort import bubble_sort
+from algorithm.sort import bubble_sort, selection_sort
 
 
-class BubbleSortTest(unittest.TestCase):
-    def test_func(self):
+class TestSort(unittest.TestCase):
+    def test_bubble(self):
         test_list = [1, 3, 5, 6, 2, 0, 8]
         sorted_list = bubble_sort(test_list)
+        self.assertEqual(sorted_list, [0, 1, 2, 3, 5, 6, 8])
+
+    def test_selection(self):
+        test_list = [1, 3, 5, 6, 2, 0, 8]
+        sorted_list = selection_sort(test_list)
         self.assertEqual(sorted_list, [0, 1, 2, 3, 5, 6, 8])
--- py/algorithm/sort.py
+++ py/algorithm/sort.py
@@ -1,4 +1,4 @@
-# 冒泡排序
+# 冒泡排序, 两层循环，依次比较相邻元素
 def bubble_sort(sort_list):
     for idx in range(len(sort_list) - 1):
         for inner in range(len(sort_list) - 1 - idx):
@@ -9,6 +9,19 @@ def bubble_sort(sort_list):
     return sort_list
 
 
+# 选择排序
+# 两层循环, 每次都与最小值进行比较
+def selection_sort(sort_list):
+    for idx in range(len(sort_list) - 2):
+        min_idx = idx
+        for inner in range(idx + 1, len(sort_list) - 1):
+            if sort_list[inner] < sort_list[min_idx]:
+                min_idx = inner
+            swap(sort_list, idx, min_idx)
+    return sort_list
+
+
+# 工具方法，交换元素位置
 def swap(sort_list, from_id, to_id):
     temp = sort_list[from_id]
     sort_list[from_id] = sort_list[to_id]

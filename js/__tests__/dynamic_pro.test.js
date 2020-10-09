a821e73c04bf3feec4566251ac01cad8f5a882ed
jqxiong <jqxiong@trip.com>
2020-10-09 19:15:54 +0800

    dynamic pro

--- js/__tests__/dynamic_pro.test.js
+++ js/__tests__/dynamic_pro.test.js
@@ -1,10 +1,16 @@
-const { fibonacci } = require('../algorithm/dynamic_pro')
+const { fibonacci, dynamicFibonacci } = require('../algorithm/dynamic_pro')
 
 describe('test dynamic', () => {
     it('test not dynamic fibonacci', () => {
         const fib1 = fibonacci(1)
         expect(fib1).toEqual(1)
         const fib6 = fibonacci(6)
+        expect(fib6).toEqual(8)
+    })
+    it('test dynamic fibo', () => {
+        const fib1 = dynamicFibonacci(1)
+        expect(fib1).toEqual(1)
+        const fib6 = dynamicFibonacci(6)
         expect(fib6).toEqual(8)
     })
 })
--- js/algorithm/dynamic_pro.js
+++ js/algorithm/dynamic_pro.js
@@ -11,6 +11,24 @@ function fibonacci(n) {
     return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
+// 动态规划实现
+function dynamicFibonacci(n) {
+    const fibArr = [];
+    if (n == 1 || n == 2) {
+        return 1
+    }
+    // 初始化一个长度为n的数组
+    for (let i = 0; i < n; i++) {
+        fibArr[i] = 0
+    }
+    fibArr[0] = fibArr[1] = 1
+    for (let i = 2; i < n; i++) {
+        fibArr[i] = fibArr[i - 1] + fibArr[i - 2]
+    }
+    return fibArr[n - 1]
+}
+
 module.exports = {
-    fibonacci
+    fibonacci,
+    dynamicFibonacci
 }

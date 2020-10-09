4abfe11cd6598ca21e9a88843907b9645595ac86
jqxiong <jqxiong@trip.com>
2020-10-09 19:25:45 +0800

    dynamic fibonacci

--- js/__tests__/dynamic_pro.test.js
+++ js/__tests__/dynamic_pro.test.js
@@ -13,4 +13,15 @@ describe('test dynamic', () => {
         const fib6 = dynamicFibonacci(6)
         expect(fib6).toEqual(8)
     })
+    // 递归的深度对js来说是巨大的, 使用递归的话，可能到50就已经算不动了
+    it('两种算法计算时间比较', () => {
+        const nDate = new Date().getTime()
+        const fibn = fibonacci(40)
+        const disN = new Date().getTime() - nDate
+        const mDate = new Date().getTime();
+        const fibm = dynamicFibonacci(40);
+        const disM = new Date().getTime() - mDate
+        expect(fibn).toEqual(fibm)
+        expect(disM).toBeLessThan(disN)
+    })
 })

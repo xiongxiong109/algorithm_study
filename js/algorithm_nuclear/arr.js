e2d35d1182c4d21b3b8af02159863c3c5ce9155b
xiongxiong109 <444122662@qq.com>
2020-10-16 22:53:08 +0800

    add

--- js/algorithm_nuclear/arr.js
+++ js/algorithm_nuclear/arr.js
@@ -1,7 +1,7 @@
 // 以下哪些表达式的值为0？
 function getZeroLen() {
     // 考点，function.length, 表示函数形参的个数
-    const len1 = (() => {}).length
+    const len1 = (() => { }).length
     // 位运算, 数字都转换成二进制, 这里做 01 AND 10 运算 => 0
     const len2 = 1 & 2
     // js弱类型之间的强制转换
@@ -32,3 +32,59 @@ function getZeroLen() {
  * 是什么 Object.prototype.toString.call()
  * 说是在iframe场景下会失效...
  */
+
+/**
+ * 以下代码执行后，array 的结果是？
+ * let array = [,1,,2,,3];
+ * array = array.map((i) => ++i)
+ * 
+ * 答案: [,2,,3,,4]
+ * map方法会自动跳过空位, 但是会保留这个元素
+ * forEach(), filter(), reduce(), every() 和some()都会跳过空位。
+ */
+
+/**
+ * 求解数组交集
+ * 如 const arr = [[1, 3], [2, 6], [8, 9], [10, 11]]
+ * 因为 [1, 3] 与 [2, 6] 的区间有交集, 所以合并成 [1, 6]
+ * conca(arr) // [[1, 6], [8, 9], [10, 11]]
+ */
+// [[1, 3], [2, 6], [5, 9], [10, 11]]
+function getCrossArr(arr) {
+
+    let rst = []
+    /**
+     * 子问题的解是: arr[n] 与 arr[n + 1] 是否有交集, 有的话做合并, 没有的话, 继续比较下一个元素
+     * 求解子问题后, 可以存储子问题得到的新数组
+     * [[1, 3], [2, 6], [5, 9], [10, 11]] =>
+     * [[1, 6], [5, 9], [10, 11]] =>
+     * [[1, 9], [10, 11]]
+     */
+    for (let i = 0; i < arr.length - 1; i++) {
+
+        const preArr = arr[i]
+        const nextArr = arr[i + 1]
+
+        const preStart = preArr[0]
+        const preEnd = preArr[1]
+        const nextStart = nextArr[0]
+        const nextEnd = nextArr[1]
+
+        if (
+            (preEnd < nextStart || preStart > nextEnd) ||
+            (nextEnd < preStart || nextStart > preEnd)
+        ) { // 没有交集, 正常对接
+            rst.push(nextArr) // [8, 9]
+        } else { // 有交集, 合并数组递归处理
+            let newRange = [
+                ...preArr,
+                ...nextArr
+            ].sort()
+            newRange = [newRange[0], newRange[newRange.length - 1]] // [1, 6]
+            rst[i] = newRange
+        }
+
+    }
+
+    console.log(rst)
+}
---
+++ js/algorithm_nuclear/exp.js
@@ -0,0 +1,14 @@
+// 正则相关
+ /**
+  * var str1=new RegExp("e");
+  * document.write(str1.exec("hello"));
+  * 以上代码的输出结果
+  * 
+  * 
+  * 考察: exp.exec方法
+  * exec方法, 如果有匹配到的元素, 则返回一个数组
+  * 如果没有匹配到，则返回null
+  * 但是这个地方, 用到了document.write, 所以答案不是一个数组, 而是"e"...
+  */
+
+  

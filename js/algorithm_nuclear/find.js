b9ab4504d1e6e1a417b1f79d5902193fd5f3a93e
xiongxiong109 <444122662@qq.com>
2020-11-20 23:01:38 +0800

    使用栈实现递归函数的非递归算法

--- js/algorithm_nuclear/find.js
+++ js/algorithm_nuclear/find.js
@@ -41,39 +41,67 @@ const sourceData = [
 ]
 
 /**
+ * 用栈实现递归算法的非递归版本
+ * 递归实现存在一个中断
+ * 栈的实现也存在一个中断, 那就是栈为空,
+ * 在这个栈为空的前提下，以while和push来模拟递归调用, 以pop来减少堆栈数据
+ */
+function find(data, id) {
+    let stack = [];
+    for (let i = 0; i < data.length; i++) {
+        let curDeep = data[i];
+        if (curDeep.id == id) {
+            return curDeep.nm
+        }
+        if (curDeep.children) {
+            stack.push(...curDeep.children);
+            while(stack.length) {
+                let curPop = stack.pop();
+                if (curPop.id == id) {
+                    return curPop.nm
+                }
+                if (curPop.children) {
+                    stack.push(...curPop.children)
+                }
+            }
+        }
+    }
+}
+
+/**
  * find(data, 111) // 川沙
  * find(data, 11) // 浦东新区
  */
 
 /**
- * 递归实现, 这个写法没有中断，总是会把循环体走完
+ * 递归实现, 这个写法没有hasFound 变量中断的话，总是会把循环体走完
  */
-function find(data, id) {
+// function find(data, id) {
 
-    let curNm = '';
-    let hasFound = false;
+//     let curNm = '';
+//     let hasFound = false;
 
-    _find(data, id)
+//     _find(data, id)
 
-    function _find(fData, fid) {
-        for (let i = 0; i < fData.length; i++) {
-            // 可以添加变量标志的方式来限制查询次数
-            if (hasFound) {
-                break
-            }
-            // console.log(fData[i].nm)
-            if (fData[i].id == fid) {
-                curNm = fData[i].nm
-                hasFound = true;
-            }
-            if (fData[i].children && fData[i].children.length) {
-                _find(fData[i].children, id);
-            }
-        }
-    }
-    return curNm
-}
+//     function _find(fData, fid) {
+//         for (let i = 0; i < fData.length; i++) {
+//             // 可以添加变量标志的方式来限制查询次数
+//             if (hasFound) {
+//                 break
+//             }
+//             // console.log(fData[i].nm)
+//             if (fData[i].id == fid) {
+//                 curNm = fData[i].nm
+//                 hasFound = true;
+//             }
+//             if (fData[i].children && fData[i].children.length) {
+//                 _find(fData[i].children, id);
+//             }
+//         }
+//     }
+//     return curNm
+// }
 
-const nm = find(sourceData, 111);
+const nm = find(sourceData, 11);
 
 console.log(nm);

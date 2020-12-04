6edc40103ee2bf11d2150be1e01ac441399f5cde
jqxiong <jqxiong@trip.com>
2020-12-04 17:42:15 +0800

    degree

---
+++ py/basic/degree.py
@@ -0,0 +1,18 @@
+# 华氏温度转换为摄氏温度
+# $C=(F - 32) \ 1.8
+
+
+# 可以使用input获取用户输入, float转换数据类型
+def trans_degree(num):
+    try:
+        c_degree = (float(num) - 32) / 1.8
+        return c_degree
+    # float可能会报错
+    except ValueError:
+        return '请输入正常数字类型'
+
+
+if __name__ == "__main__":
+    rst = input("请输入华氏温度:\n")
+    degree = trans_degree(rst)
+    print(degree)

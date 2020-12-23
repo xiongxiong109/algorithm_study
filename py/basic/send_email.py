e02b521e2db5f15f0c1ecc59412fe3334975cd11
jqxiong <444122662@qq.com>
2020-12-23 21:03:23 +0800

    auth email

--- py/basic/send_email.py
+++ py/basic/send_email.py
@@ -6,8 +6,8 @@ from email.mime.text import MIMEText
 
 # 发送邮件功能
 def send_email():
-    sender = '444122662@qq.com'
-    receivers = ['jqxiong@trip.com']
+    sender = 'bearcambridge@qq.com'
+    receivers = ['444122662@qq.com']
 
     message = MIMEText('用Python发送邮件的示例代码.', 'plain', 'utf-8')
     message['From'] = Header('jqxiong', 'utf-8')
@@ -16,10 +16,12 @@ def send_email():
     message['Subject'] = Header('示例代码实验邮件', 'utf-8')
     smtper = SMTP('smtp.qq.com')
 
-    # 请自行修改下面的登录口令
-    smtper.login(sender, 'pass')
-    smtper.sendmail(sender, receivers, message.as_string())
-    print('邮件发送完成!')
+    # SMTP发送邮件，如果使用QQ邮箱，需要设置独立账号授权码
+    with open('./__temp/email_auth', 'r', encoding='utf-8') as f:
+        auth_key = f.read().strip()
+        smtper.login(sender, auth_key)
+        smtper.sendmail(sender, receivers, message.as_string())
+        print('邮件发送完成!')
 
 
 if __name__ == '__main__':

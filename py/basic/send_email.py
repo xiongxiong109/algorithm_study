c92422f7055c8b7abd0667577f1ca3b6762bcd9b
jqxiong <444122662@qq.com>
2020-12-23 16:32:16 +0800

    send_email

---
+++ py/basic/send_email.py
@@ -0,0 +1,26 @@
+# 基于SMTP的邮件传输功能
+from smtplib import SMTP
+from email.header import Header
+from email.mime.text import MIMEText
+
+
+# 发送邮件功能
+def send_email():
+    sender = '444122662@qq.com'
+    receivers = ['jqxiong@trip.com']
+
+    message = MIMEText('用Python发送邮件的示例代码.', 'plain', 'utf-8')
+    message['From'] = Header('jqxiong', 'utf-8')
+    message['To'] = Header('xiongxiong109', 'utf-8')
+
+    message['Subject'] = Header('示例代码实验邮件', 'utf-8')
+    smtper = SMTP('smtp.qq.com')
+
+    # 请自行修改下面的登录口令
+    smtper.login(sender, 'pass')
+    smtper.sendmail(sender, receivers, message.as_string())
+    print('邮件发送完成!')
+
+
+if __name__ == '__main__':
+    send_email()

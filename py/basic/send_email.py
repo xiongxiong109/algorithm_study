5936d11cf09226e362aff64f94ca434ad711d01c
jqxiong <jqxiong@trip.com>
2020-12-24 10:39:17 +0800

    send multipart

--- py/basic/send_email.py
+++ py/basic/send_email.py
@@ -2,6 +2,7 @@
 from smtplib import SMTP
 from email.header import Header
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 # 发送邮件功能
@@ -24,5 +25,36 @@ def send_email():
         print('邮件发送完成!')
 
 
+# 发送带附件的邮件
+def send_email_mult():
+
+    # 创建带附件的邮件实例
+    message = MIMEMultipart()
+    message['Subject'] = Header('附件发送', 'utf-8')
+    message['From'] = Header('Hicker', 'utf-8')
+    message['To'] = Header('humanbeings', 'utf-8')
+
+    with open('./__temp/email_auth', 'r', encoding='utf-8') as f:
+        # 把auth key 作为文本发送出去
+        auth_key = f.read().strip()
+        # 设置附件内容
+        txt = MIMEText('授权码：{0}'.format(auth_key), 'base64', 'utf-8')
+        txt['Content-Type'] = 'text/plain'
+        txt['Content-Disposition'] = 'attachment; filename=auth.txt'
+        # 将附件挂在到message对象
+        message.attach(txt)
+
+        greeting = MIMEText('\nSir, the auth key here', 'plain', 'utf-8')
+        message.attach(greeting)
+
+        smtper = SMTP('smtp.qq.com')
+        sender = 'bearcambridge@qq.com'
+        smtper.login(sender, auth_key)
+        smtper.sendmail(sender, ['444122662@qq.com'], message.as_string())
+        smtper.quit()
+        print('email sended')
+
+
 if __name__ == '__main__':
-    send_email()
+    # send_email()
+    send_email_mult()
